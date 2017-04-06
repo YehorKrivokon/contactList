4d90c09418770f6548f4a5c38735536dfaa508e0
Yehor123 <ekrivokon@inbox.ru>
2017-04-06 16:23:06 +0300

    fixed bug with /welcome access

--- src/java/contactList/app/controller/UserController.java
+++ src/java/contactList/app/controller/UserController.java
@@ -5,6 +5,8 @@ import contactList.app.service.SecurityService;
 import contactList.app.service.UserService;
 import contactList.app.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -61,7 +63,11 @@ public class UserController {
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcome(Model model) {
-        return "welcome";
+        String result = "welcome";
+        if (SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken){
+            result = "login";
+        }
+        return result;
     }
 
 }
